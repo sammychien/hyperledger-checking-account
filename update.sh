27a53af844b8782e20a86bc3bd849fb6290f4e45
Maohua Wang <maohua.ethan.wang@utexas.edu>
2021-04-29 03:34:34 -0500

    add update shell

---
+++ update.sh
@@ -0,0 +1,3 @@
+cp ~/hyperledger-checking-account/fabric-samples/checking-account/chaincode-js/lib/assetTransfer.js ~/hyperledgerfabric/fabric-samples/asset-transfer-basic/chaincode-javascript/lib/assetTransfer.js 
+cp ~/hyperledger-checking-account/fabric-samples/checking-account/app-js/app.js ~/hyperledgerfabric/fabric-samples/asset-transfer-basic/application-javascript/app.js
+
